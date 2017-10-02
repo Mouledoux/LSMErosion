1c1feaaf28a2646461c8ed0d7954a9f69fe68633
Eric Mouledoux <EricMouledoux@gmail.com>
2017-10-02 09:09:34 -0500

    Removed unnecessary sdk

---
+++ Assets/Scripts/CutModel.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CutModel : MonoBehaviour
+{
+	void Start ()
+    {
+        Vector3[] verts = GetComponent<MeshFilter>().mesh.vertices;
+        Color[] colors = new Color[verts.Length];
+
+        int h, l;
+        h = l = 0;
+
+        for (int i = 0; i < verts.Length; i++)
+        {
+            int floor = Mathf.FloorToInt(verts[i].y);
+            floor += floor % 10;
+            verts[i].y = floor;
+
+            h = floor > h ? floor : h;
+            l = floor < l ? floor : l;
+
+            colors[i].r = Mathf.InverseLerp(h, l, floor);
+            colors[i].g = Mathf.InverseLerp(l, h, floor);
+            colors[i].b = 0f;
+            colors[i].a = 1f;
+        }
+
+        GetComponent<MeshFilter>().mesh.vertices = verts;
+        GetComponent<MeshFilter>().mesh.colors = colors;
+
+    }
+}
