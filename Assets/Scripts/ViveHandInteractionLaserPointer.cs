f31a0299e11ef7ecba55671ac620742c5fc3c4b2
Eric Mouledoux <EricMouledoux@gmail.com>
2017-10-05 10:29:48 -0500

    Laserpointer placment scripts

--- Assets/Scripts/ViveHandInteractionLaserPointer.cs
+++ Assets/Scripts/ViveHandInteractionLaserPointer.cs
@@ -13,20 +13,30 @@ public class ViveHandInteractionLaserPointer : MonoBehaviour
     private Valve.VR.InteractionSystem.Hand m_hand;
     private LineRenderer m_lineRenderer;
 
-
 	void Start ()
     {
         m_hand = GetComponent<Valve.VR.InteractionSystem.Hand>();
         m_lineRenderer = GetComponent<LineRenderer>();
 	}
-	
 
 	void Update ()
     {
         if (CheckObjectHit())
-            if(CheckObject())
-                if(CheckInput())
+        {
+            Mouledoux.Components.Mediator.instance.NotifySubscribers
+                (m_raycast.transform.gameObject.GetInstanceID().ToString() + "->highlight", new Mouledoux.Callback.Packet());
+
+            if (CheckObject())
+            {
+                if (CheckInput())
+                {
+                    Mouledoux.Components.Mediator.instance.NotifySubscribers
+                        (m_raycast.transform.gameObject.GetInstanceID().ToString() + "->interact", new Mouledoux.Callback.Packet());
+
                     PickUpObject();
+                }
+            }
+        }
 
         UpdateLaser();
     }
@@ -35,17 +45,6 @@ public class ViveHandInteractionLaserPointer : MonoBehaviour
     public bool CheckObjectHit()
     {
         return (Physics.Raycast(m_hand.transform.position, m_hand.transform.forward, out m_raycast));
-
-        if (!Physics.Raycast(m_hand.transform.position, m_hand.transform.forward, out m_raycast)) return false;
-        else if (!m_raycast.transform.gameObject.GetComponent<Valve.VR.InteractionSystem.Interactable>()) return false;
-        else if (!m_raycast.transform.gameObject.CompareTag(m_hitTag)) return false;
-
-        else
-        {
-            Mouledoux.Components.Mediator.instance.
-                NotifySubscribers(m_raycast.transform.gameObject.GetInstanceID().ToString(), new Mouledoux.Callback.Packet());
-            return true;
-        }
     }
 
     public bool CheckObject()
@@ -53,39 +52,34 @@ public class ViveHandInteractionLaserPointer : MonoBehaviour
         return m_raycast.transform.gameObject.CompareTag(m_hitTag);
     }
 
-    public void UpdateLaser()
-    {
-        Vector3 endPoint = CheckObjectHit() ? m_raycast.point : m_hand.transform.position + m_hand.transform.forward;
-        
-        Vector3[] v = { m_hand.transform.position, endPoint };
-        m_lineRenderer.SetPositions(v);
-
-        if (m_hand.AttachedObjects.Count > 1)
-            m_hand.AttachedObjects[1].attachedObject.transform.position = endPoint;
-    }
-
-
     public bool CheckInput()
     {
         return (m_hand.controller.GetHairTriggerDown());
     }
 
-
     public int PickUpObject()
     {
-        //m_hand.AttachObject(m_raycast.transform.gameObject);
-        StartCoroutine(HoldObject(m_raycast.transform.gameObject));
+        StartCoroutine(HoldObject());
         return 0;
     }
 
-    public System.Collections.IEnumerator HoldObject(GameObject target)
+    public void UpdateLaser()
     {
-        target.GetComponent<Collider>().enabled = false;
+        m_lineRenderer.SetPositions( new Vector3[] {m_hand.transform.position, m_raycast.point});
+    }
+
+    public System.Collections.IEnumerator HoldObject()
+    {
+        Transform t = m_raycast.transform;
+        Collider c = m_raycast.collider;
+
+        c.enabled = false;
+
         while(m_hand.controller.GetHairTrigger())
         {
-            target.transform.position = m_raycast.point;
             yield return null;
         }
-        target.GetComponent<Collider>().enabled = true;
+
+        c.enabled = true;
     }
 }
