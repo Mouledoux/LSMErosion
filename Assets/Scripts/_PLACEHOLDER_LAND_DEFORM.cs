541ebd4aa6ccd5930535486c4d2e6d8e5b0cdd46
Eric Mouledoux <EricMouledoux@gmail.com>
2017-10-06 12:44:21 -0500

    Deformation prototype working

--- Assets/Scripts/_PLACEHOLDER_LAND_DEFORM.cs
+++ Assets/Scripts/_PLACEHOLDER_LAND_DEFORM.cs
@@ -5,23 +5,61 @@ using UnityEngine;
 [RequireComponent(typeof(MeshFilter), typeof(MeshCollider))]
 public class _PLACEHOLDER_LAND_DEFORM : MonoBehaviour
 {
-    public GameObject sphere;
     private Mesh m_mesh;
+    private MeshCollider m_collider;
     
 	void Start ()
     {
         m_mesh = GetComponent<MeshFilter>().mesh;
-        //Dictionary<GameObject, List<ref Vector3>> verticies = new Dictionary<GameObject, List<ref Vector3>>();
+        m_collider = GetComponent<MeshCollider>();
 
-        foreach (Vector3 vert in m_mesh.vertices)
-        {
+    }
 
-        }
-
-	}
-	
-	void Update ()
+    void Update()
     {
 
-	}
+    }
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        Rigidbody rb = collision.gameObject.GetComponent<Rigidbody>();
+        if (rb == null) return;
+    
+        
+
+        Vector3[] vertices = m_mesh.vertices;
+
+        float near, far;
+        float dist = 0f;
+        near = float.MaxValue;
+        far = float.MinValue;
+
+        Vector3 POC = collision.ClosestPoint(transform.position);
+        //foreach (ContactPoint point in collision.contacts)
+        //{ POC += point.point; } POC /= collision.contacts.Length;
+
+        for (int i = 0; i < vertices.Length; i++)
+        {
+            dist = Vector3.Distance(vertices[i], POC);
+            near = dist < near ? dist : near;
+            far = dist > far ? dist : far;
+        }
+
+        for (int i = 0; i < vertices.Length; i++)
+        {
+            dist = Vector3.Distance(transform.position + vertices[i], POC);
+
+            if (dist <= 0.5f)
+            {
+                vertices[i] += rb.velocity.normalized;
+            }
+        }
+
+
+        m_mesh.vertices = vertices;
+        m_collider.sharedMesh = m_mesh;
+
+        Destroy(collision.gameObject);
+    }
 }
+
