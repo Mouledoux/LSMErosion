29e3ddade81f031af19383bba4e05dfaa8a50355
Eric Mouledoux <EricMouledoux@gmail.com>
2017-10-06 10:45:51 -0500

    Unity standard asset water

---
+++ Assets/Scripts/_PLACEHOLDER_LAND_DEFORM.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter), typeof(MeshCollider))]
+public class _PLACEHOLDER_LAND_DEFORM : MonoBehaviour
+{
+    public GameObject sphere;
+    private Mesh m_mesh;
+    
+	void Start ()
+    {
+        m_mesh = GetComponent<MeshFilter>().mesh;
+        //Dictionary<GameObject, List<ref Vector3>> verticies = new Dictionary<GameObject, List<ref Vector3>>();
+
+        foreach (Vector3 vert in m_mesh.vertices)
+        {
+
+        }
+
+	}
+	
+	void Update ()
+    {
+
+	}
+}
