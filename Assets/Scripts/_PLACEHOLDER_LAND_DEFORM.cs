a20c379640e56a57d87fd7ff97e0c52a2cfb9f5c
Eric Mouledoux <EricMouledoux@gmail.com>
2017-10-10 11:02:39 -0500

    Removed useless enumerators

--- Assets/Scripts/_PLACEHOLDER_LAND_DEFORM.cs
+++ Assets/Scripts/_PLACEHOLDER_LAND_DEFORM.cs
@@ -20,7 +20,7 @@ public class _PLACEHOLDER_LAND_DEFORM : MonoBehaviour
         if (rb == null) return;
 
         Vector3[] vertices = m_mesh.vertices;
-        
+
         float dist = 0f;
         
         Vector3 POC = other.ClosestPoint(transform.position);
@@ -32,6 +32,8 @@ public class _PLACEHOLDER_LAND_DEFORM : MonoBehaviour
             if (dist <= 1)
             {
                 vertices[i] += transform.InverseTransformDirection(rb.velocity.normalized * rb.mass) * Mathf.Abs(dist - 1);
+                //Vector3 newVert = vertices[i] + transform.InverseTransformDirection(rb.velocity.normalized * rb.mass) * Mathf.Abs(dist - 1);
+                //StartCoroutine(SmoothLandDeform(i, newVert));
             }
         }
         
@@ -39,6 +41,21 @@ public class _PLACEHOLDER_LAND_DEFORM : MonoBehaviour
         m_collider.sharedMesh = m_mesh;
 
         Destroy(other.gameObject);
+    }
+
+    public IEnumerator SmoothLandDeform(int vertIndex, Vector3 newVert)
+    {
+        Vector3[] vertices = m_mesh.vertices;
+
+        while (Vector3.Distance(vertices[vertIndex], newVert) > 0.01f)
+        {
+            vertices[vertIndex] = Vector3.Lerp(vertices[vertIndex], newVert, Time.deltaTime);
+
+            m_mesh.vertices = vertices;
+            m_collider.sharedMesh = m_mesh;
+
+            yield return null;
+        }
     }
 }
 
