fe4fedafcd25bdf84f69e1f331c916cd13ef6f46
Eric Mouledoux <EricMouledoux@gmail.com>
2017-10-11 10:01:17 -0500

    Kill wall for enemy testing

---
+++ Assets/Scripts/KillWall.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillWall : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(other.gameObject);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Destroy(collision.gameObject);
+    }
+}
