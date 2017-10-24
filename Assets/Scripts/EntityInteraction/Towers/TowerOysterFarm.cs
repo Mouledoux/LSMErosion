8e90a8b7aff2e643fc8657bd823d8ad3d8ec919e
Eric Mouledoux <EricMouledoux@gmail.com>
2017-10-24 16:23:43 -0500

    added power books

---
+++ Assets/Scripts/EntityInteraction/Towers/TowerOysterFarm.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerOysterFarm : MonoBehaviour
+{
+    public int m_health;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Rigidbody rb = other.GetComponent<Rigidbody>();
+
+        if (rb == null) return;
+
+        other.enabled = false;
+        m_health--;
+
+
+        if (m_health <= 0) Destroy(gameObject);
+    }
+}
