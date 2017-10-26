a8f42b4d3fa0374d57a3c995f4fe0e31b9046c47
Eric Mouledoux <EricMouledoux@gmail.com>
2017-10-26 16:58:06 -0500

    Audio for wind and iysters

--- Assets/Scripts/RigidbodyPush.cs
+++ Assets/Scripts/RigidbodyPush.cs
@@ -7,7 +7,6 @@ public class RigidbodyPush : MonoBehaviour
     public float force = 0.0f;
     public Vector3 direction;
 
-
 	void Start ()
     {
         Rigidbody rb = GetComponent<Rigidbody>();
@@ -31,7 +30,13 @@ public class RigidbodyPush : MonoBehaviour
         }
 
         if (nScale == Vector3.zero)
+        {
+            GetComponent<Collider>().enabled = false;
+
+            yield return new WaitWhile(() => GetComponent<AudioSource>().isPlaying);
+
             Destroy(gameObject);
+        }
     }
 
     private void OnTriggerEnter(Collider other)
@@ -40,7 +45,13 @@ public class RigidbodyPush : MonoBehaviour
             other.gameObject.layer == LayerMask.NameToLayer("Water") ||
             (!other.CompareTag(tag) && other.gameObject.layer != LayerMask.NameToLayer("Land"))) return;
 
+        AudioSource audio = GetComponent<AudioSource>();
+        audio.loop = false;
+        audio.Play();
+
         StopAllCoroutines();
         StartCoroutine(GrowShrink(transform.localScale, Vector3.zero));
+
+
     }
 }
