642735ddff79e8730a42dac1fb949d26f7f9e126
Zac King <ZacKingx@gmail.com>
2017-10-31 14:24:19 -0500

    push for 3pm build

---
+++ Assets/AnimateScoreCard.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimateScoreCard : MonoBehaviour
+{
+    [SerializeField] AnimationCurve m_growthAnimation;
+    [SerializeField] float m_animateTime = 0;
+
+    IEnumerator AnimateScore()
+    {
+        float t = 0;
+        while(t < m_animateTime)
+        {
+            t += Time.deltaTime;
+        }
+        return null;
+    }
+}
