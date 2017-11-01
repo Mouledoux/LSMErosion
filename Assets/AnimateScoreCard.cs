930352c0ff8d8cc0f843278174e69fac9c27a010
Zac King <ZacKingx@gmail.com>
2017-11-01 12:05:41 -0500

    scorecard prefab

--- Assets/AnimateScoreCard.cs
+++ Assets/AnimateScoreCard.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -7,17 +8,23 @@ public class AnimateScoreCard : MonoBehaviour
     [SerializeField] AnimationCurve m_growthAnimation;
     [SerializeField] float m_animateTime = 0;
     [SerializeField] Text m_scoretext;
+    [SerializeField] List<GameObject> m_Fireworks;
 
     [ContextMenu("Animate")]
     public void _test()
     {
-        StartCoAnim("Congratulation \n\n\n[Insert title]\n\n\nYou have protected the coast");
+        AnimScoreCard("Congratulation \n\n\n[Insert title]\n\n\nYou have protected the coast");
     }
 
 
-    public void StartCoAnim(string s)
+    public void AnimScoreCard(string s)
     {
         m_scoretext.text = s;
+        foreach(GameObject g in m_Fireworks)
+        {
+            g.SetActive(true);
+        }
+
         StartCoroutine(AnimateScore());
     }
 
@@ -28,7 +35,8 @@ public class AnimateScoreCard : MonoBehaviour
         while(t <= m_animateTime)
         {
             Vector3 v = Vector3.zero;
-            v.x = v.y = m_growthAnimation.Evaluate(t/m_animateTime);
+            v.x = v.y = v.z = m_growthAnimation.Evaluate(t/m_animateTime);
+            
             gameObject.transform.localScale = v;
 
             t += Time.deltaTime;
