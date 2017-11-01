f2d8cce0ab90981d750b094dfb3ac7a002f89fbb
Zac King <ZacKingx@gmail.com>
2017-11-01 10:34:29 -0500

    Book prefab update
    Correct and incorrect answers highlight and trigger sfx before
    progressing

--- Assets/AnimateScoreCard.cs
+++ Assets/AnimateScoreCard.cs
@@ -1,19 +1,39 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class AnimateScoreCard : MonoBehaviour
 {
     [SerializeField] AnimationCurve m_growthAnimation;
     [SerializeField] float m_animateTime = 0;
+    [SerializeField] Text m_scoretext;
 
-    IEnumerator AnimateScore()
+    [ContextMenu("Animate")]
+    public void _test()
+    {
+        StartCoAnim("Congratulation \n\n\n[Insert title]\n\n\nYou have protected the coast");
+    }
+
+
+    public void StartCoAnim(string s)
+    {
+        m_scoretext.text = s;
+        StartCoroutine(AnimateScore());
+    }
+
+
+    public IEnumerator AnimateScore()
     {
         float t = 0;
-        while(t < m_animateTime)
+        while(t <= m_animateTime)
         {
+            Vector3 v = Vector3.zero;
+            v.x = v.y = m_growthAnimation.Evaluate(t/m_animateTime);
+            gameObject.transform.localScale = v;
+
             t += Time.deltaTime;
+
+            yield return null;
         }
-        return null;
     }
 }
---
+++ Assets/QuizAnswerColor.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class QuizAnswerColor : MonoBehaviour
+{
+    public Color m_color;
+    
+    public void ColorizeAnswer()
+    {
+        gameObject.GetComponent<SpriteRenderer>().color = m_color;
+    }
+}
